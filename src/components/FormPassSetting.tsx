2ec77b882e03445bae35000bbd75ec1e30ae160d
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-08-02 17:33:30 +0300

    Add the form password generator

--- src/components/FormPassSetting.tsx
+++ src/components/FormPassSetting.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import RangeSlider from "./RangeSlider";
 
 const FormPassSetting = () => {
   return (
-    <div className="bg-primary w-5/6 h-[522px] mx-auto">
-      <h1>FormPassSetting</h1>
+    <div className="bg-primary w-5/6 h-[522px] mx-auto px-5 py-4">
+      <RangeSlider />
     </div>
   );
 };
