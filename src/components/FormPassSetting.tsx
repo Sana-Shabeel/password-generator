c8206d85c6fb454a7ad6e551008b902b71e04019
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-08-14 21:02:26 +0300

    Add checkboxes

--- src/components/FormPassSetting.tsx
+++ src/components/FormPassSetting.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import RangeSlider from "./RangeSlider";
+import Checkbox from "./Checkbox";
 
 const FormPassSetting = () => {
+  const [uppercase, setuppercase] = useState(false);
+  const [lowercase, setLowercase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+
   return (
     <div className="bg-primary w-5/6 h-[522px] mx-auto px-5 py-4">
       <RangeSlider />
+      <div className="flex flex-col gap-3 mt-3">
+        <Checkbox
+          label="Include Uppercase Letters"
+          checked={uppercase}
+          setChecked={setuppercase}
+        />
+        <Checkbox
+          label="Include Lowercase Letters"
+          checked={lowercase}
+          setChecked={setLowercase}
+        />
+        <Checkbox
+          label="Include Numbers"
+          checked={numbers}
+          setChecked={setNumbers}
+        />
+        <Checkbox
+          label="Include Symbols"
+          checked={symbols}
+          setChecked={setSymbols}
+        />
+      </div>
     </div>
   );
 };
