0e4c3470fdc2e9ad7a69681017b96944cd2896de
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-09-18 14:05:06 +0300

    Add pass gen

--- src/components/DisplayPassword.tsx
+++ src/components/DisplayPassword.tsx
@@ -1,11 +1,15 @@
-const DisplayPassword = () => {
+interface DisplayPasswordProps {
+  generatedPassword: string;
+}
+
+const DisplayPassword = ({ generatedPassword }: DisplayPasswordProps) => {
   return (
-    <div className="bg-primary h-24 px-3 md:h-32  w-5/6 flex justify-between items-center mx-auto mb-8 max-w-lg">
+    <div className="bg-primary h-20 px-3 md:h-32  w-5/6 flex justify-between items-center mx-auto mb-8 max-w-lg">
       <span
         id="display-password"
         className="text-textColor text-2xl md:text-4xl opacity-40 font-bold "
       >
-        P4$5W0rD!
+        {generatedPassword || "P4$5W0rD!"}
       </span>
       <div>
         <img
