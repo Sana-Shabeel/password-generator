7fa6dbcdf39c1d5bbc4a2127afb2cfc460dbe9cc
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-09-25 18:30:15 +0300

    signed

--- src/components/DisplayPassword.tsx
+++ src/components/DisplayPassword.tsx
@@ -3,7 +3,7 @@ interface DisplayPasswordProps {
 }
 
 const DisplayPassword = ({ generatedPassword }: DisplayPasswordProps) => {
-  // is this a signed commit ????
+  // this is a component that displays the generated password
   return (
     <div className="bg-primary h-20 px-3 md:h-32  w-5/6 flex justify-between items-center mx-auto mb-8 max-w-lg">
       <span
