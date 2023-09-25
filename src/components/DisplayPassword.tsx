4d7257809570e167d481b87529f572315c00c648
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-09-25 17:38:26 +0300

    Add log message

--- src/components/DisplayPassword.tsx
+++ src/components/DisplayPassword.tsx
@@ -3,6 +3,8 @@ interface DisplayPasswordProps {
 }
 
 const DisplayPassword = ({ generatedPassword }: DisplayPasswordProps) => {
+  console.log(generatedPassword);
+
   return (
     <div className="bg-primary h-20 px-3 md:h-32  w-5/6 flex justify-between items-center mx-auto mb-8 max-w-lg">
       <span
